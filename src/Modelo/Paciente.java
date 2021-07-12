6c4a2b4ba84443f6ea18f72fd77112e52e618119
Eduardo Consalvo <educon001@gmailcom>
2021-07-11 21:27:41 -0400

    Cambio a paciente

--- src/Modelo/Paciente.java
+++ src/Modelo/Paciente.java
@@ -5,34 +5,27 @@ import java.util.ArrayList;
 
 public class Paciente extends Persona{
     
-    private boolean alDia;
     private ArrayList<Pago> pagos;
 
     public Paciente() {
         pagos = new ArrayList<>();
     }
 
-    public boolean isAlDia() {
-        return alDia;
-    }
-
-    public void setAlDia(boolean alDia) {
-        this.alDia = alDia;
-    }
-
     public ArrayList<Pago> getPagos() {
         return pagos;
     }
     
-   public void registrarPago(Pago pago){
+    public void registrarPago(Pago pago){
        pagos.add(pago);
-   }
+    }
     
-   public Pago buscarPago(long fact){
+    public Pago buscarPago(long fact){
         for (Pago pago: pagos)
             if (pago.getFactura() == fact)
                 return pago;
         return null;
-   }
+    }
     
+    //public boolean alDia(){ Hay que hacer este método
+       
 }

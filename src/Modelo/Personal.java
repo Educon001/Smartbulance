11e6b2337e9bf099d626976680f56e874b884e9f
Eduardo Consalvo <educon001@gmailcom>
2021-07-12 19:34:47 -0400

    Personal

---
+++ src/Modelo/Personal.java
@@ -0,0 +1,12 @@
+
+package Modelo;
+
+import java.util.Date;
+
+public class Personal {
+    
+    protected long numCarnet;
+    protected boolean activo;
+    protected double salario;
+    protected Date fechaContrato;
+}

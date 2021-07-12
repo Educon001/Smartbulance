d8e612e4d880efa3c71e02acbcaa53b6506a68fd
Educon001 <86416036+Educon001@users.noreply.github.com>
2021-07-12 17:36:59 -0400

    Prueba

--- src/Modelo/Vehiculo.java
+++ src/Modelo/Vehiculo.java
@@ -3,4 +3,17 @@ package Modelo;
 
 public class Vehiculo {
     //Hola marco
+    
+    
+    
+    
+    
+    //hola otra vez
+    
+    
+    //holis
+    
+    
+    
+    //Marico el que lo lea
 }

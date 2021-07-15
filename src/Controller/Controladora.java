1368004e2a02ecb6562d8199d23d1b5e0a4b5a14
Eduardo Consalvo <educon001@gmailcom>
2021-07-14 20:59:09 -0400

    Los cambios de Jesús a la interfaz

--- src/Controller/Controladora.java
+++ src/Controller/Controladora.java
@@ -1,6 +1,25 @@
 
 package Controller;
 
+import javax.swing.*;
+import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
+
 public class Controladora {
+
+    JFrame ventana;
+    
+    public Controladora(JFrame ventana) {
+        this.ventana=ventana;        
+    }
+    
+    public void iniciaVentana(JFrame ventana, String ruta){
+       ventana.setLocationRelativeTo(null);
+       ventana.setIconImage(new ImageIcon(ruta).getImage()); 
+       ventana.setResizable(false); 
+       ventana.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+    }
+    
+    
+    
     
 }

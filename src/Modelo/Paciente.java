32d7e1e96aa114f722e8307f92d628654f35e5de
Educon001 <86416036+Educon001@users.noreply.github.com>
2021-07-21 20:37:21 -0400

    C paciente (#16)
    * CPaciente y modificacion a paciente
    
    * pinche nullpointerException
    
    * Agregar y ver pacientes
    
    Co-authored-by: Marco <castroo8a2@gmail.com>

--- src/Modelo/Paciente.java
+++ src/Modelo/Paciente.java
@@ -1,16 +1,23 @@
 
 package Modelo;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
+
+
 
 public class Paciente extends Persona{
     
     private ArrayList<Pago> pagos;
 
-    public Paciente() {
-        super();
+   
+    public Paciente(String cedula, String nombre, String correo, String telefono, LocalDate nacimiento, char genero) {
+        super( cedula,  nombre,  correo,  telefono,  nacimiento,  genero);        
         pagos = new ArrayList<>();
-    }
+    }  
+
+   
 
     public ArrayList<Pago> getPagos() {
         return pagos;
@@ -25,6 +32,7 @@ public class Paciente extends Persona{
             if (pago.getFactura() == fact)
                 return pago;
         return null;
+        
     }
     
     //public boolean alDia(){ Hay que hacer este método

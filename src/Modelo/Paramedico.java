468dd8d3450c67fb19cf06c7752755cc78dcaf24
yesus <jesusdfermin01@gmail.com>
2021-07-22 01:22:35 -0700

    Ahora se ven los turnos del personal

--- src/Modelo/Paramedico.java
+++ src/Modelo/Paramedico.java
@@ -9,8 +9,8 @@ public class Paramedico extends PersonalConVehiculo {
 
     
 
-    public Paramedico(long numCarnet, boolean activo,double salario,LocalDate fechaContrato,String tipo,String cedula,String nombre,String correo,String telefono,LocalDate nacimiento,char genero,String vehActual,boolean asignadoDRS) {
-        super(vehActual,numCarnet, activo, salario, fechaContrato, tipo, cedula, nombre, correo, telefono, nacimiento, genero);
+    public Paramedico(long numCarnet, boolean activo,double salario,LocalDate fechaContrato,String tipo,String cedula,String nombre,String correo,String telefono,LocalDate nacimiento,char genero,boolean asignadoDRS) {
+        super(numCarnet, activo, salario, fechaContrato, tipo, cedula, nombre, correo, telefono, nacimiento, genero);
         this.asignadoDRS = asignadoDRS;
     }
 
